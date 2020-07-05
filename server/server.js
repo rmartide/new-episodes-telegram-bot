b6057a4b64bf581fe68cc73c4f0395d5ea9690f2
Rodrigo Martín de Lamo <mcgod69esp@gmail.com>
2020-07-05 19:09:40 +0200

    Trying node-telegram-bot-api

--- server/server.js
+++ server/server.js
@@ -1,8 +1,7 @@
-const express = require('express')
-const app = express()
- 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
- 
-app.listen(3000)
+const TelegramBot = require('node-telegram-bot-api');
+require('dotenv').config();
+const token = process.env.BOT_API;
+const chatid = process.env.CHAT_ID;
+const bot = new TelegramBot(token, {polling: true});
+
+bot.sendMessage(chatid, "Yo wadup");
