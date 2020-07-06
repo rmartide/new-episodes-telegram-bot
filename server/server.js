e358544d5552c1ea076459a3d5e026ce96ea6184
Rodrigo Martín de Lamo <mcgod69esp@gmail.com>
2020-07-06 21:00:58 +0200

    Added node-schedule and programmed a schedule to execute a task

--- server/server.js
+++ server/server.js
@@ -1,7 +1,22 @@
-const TelegramBot = require('node-telegram-bot-api');
-require('dotenv').config();
-const token = process.env.BOT_API;
-const chatid = process.env.CHAT_ID;
-const bot = new TelegramBot(token, {polling: true});
+process.env.NTBA_FIX_319 = 1;
 
-bot.sendMessage(chatid, "Yo wadup");
+const TelegramBot = require("node-telegram-bot-api");
+const schedule = require("node-schedule");
+require("dotenv").config();
+
+const botapi = process.env.BOT_API;
+const chatid = process.env.CHAT_ID;
+
+const bot = new TelegramBot(botapi, { polling: true });
+
+const rule = new schedule.RecurrenceRule();
+
+rule.hour = new schedule.Range(16, 22);
+rule.minute = [0, 15, 30, 45];
+
+schedule.scheduleJob(rule, () => {
+	bot.sendMessage(
+		chatid,
+		`Yo wadup, it should be between 16:00 and 22:00 and a multiple of 15 minutes ${new Date().getHours()}:${new Date().getMinutes()}`
+	);
+});
