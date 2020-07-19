6883986e5e768c5fe7245145a3d11aa6d8afa2e2
Rodrigo Martín de Lamo <mcgod69esp@gmail.com>
2020-07-19 20:36:29 +0200

    Changing unknown function to console.log

--- server/mongoose/connect.js
+++ server/mongoose/connect.js
@@ -2,5 +2,5 @@ const mongoose = require('mongoose');
 const mongoPort = process.env.MONGO_PORT || 27017;
 mongoose.connect(`mongodb://localhost:${mongoPort}/Episodes`, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connection.on('error', err => {
-    logError(err);
+    console.log(err);
   });
