5e3b22bc4d4d816d93bf056a2c34bb691ff6ef71
Rodrigo Martín de Lamo <mcgod69esp@gmail.com>
2020-07-22 20:19:43 +0200

    Adding log for connection to mongo

--- server/mongoose/connect.js
+++ server/mongoose/connect.js
@@ -1,7 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const mongoPort = process.env.MONGO_PORT || 27017;
 const host = process.env.HOST;
-mongoose.connect(`mongodb://${host}:${mongoPort}/Episodes`, { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.connection.on('error', err => {
-    console.log(err);
-  });
+mongoose
+	.connect(`mongodb://${host}:${mongoPort}/Episodes`, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() => {
+		console.log("connected to mongodb");
+	})
+	.catch((err) => {
+		console.log(err);
+	});
