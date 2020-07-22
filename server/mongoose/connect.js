989dd92b5d327d6947a893564275b79360136eac
Rodrigo Martín de Lamo <mcgod69esp@gmail.com>
2020-07-22 20:02:26 +0200

    Changing mongoose connection url to accept env variables

--- server/mongoose/connect.js
+++ server/mongoose/connect.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const mongoPort = process.env.MONGO_PORT || 27017;
-mongoose.connect(`mongodb://localhost:${mongoPort}/Episodes`, { useNewUrlParser: true, useUnifiedTopology: true });
+const host = process.env.HOST;
+mongoose.connect(`mongodb://${host}:${mongoPort}/Episodes`, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connection.on('error', err => {
     console.log(err);
   });
